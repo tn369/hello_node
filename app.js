fe347ba854822f88808816417e5c91977e9dfcae
nakatomi <nakatomi369@gmial.com>
2022-06-15 00:37:01 +0900

    clean use async,await

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const http = require('http');
-const fs = require('fs');
+const fs = require('fs').promises;
 
-const server = http.createServer((req, res) => {
-    fs.readFile('index.html', 'utf-8', (err, data) => {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(data);
-        res.end();
-    });
-});
-
+const server = http.createServer(getFromClient);
 server.listen(3000);
 console.log("Server start!")
+
+async function getFromClient(req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    const content = await fs.readFile('index.html', 'utf-8')
+    res.write(content);
+    res.end();
+}
