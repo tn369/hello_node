ea125fd4af9d9049a98c41dc2748583696c2556c
nakatomi <nakatomi369@gmial.com>
2022-06-15 00:22:14 +0900

    change read html file

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<!DOCTYPE > <html lang="ja">')
-    res.write('<head><meta charset="utf-8"></head>')
-    res.write('<title>Hello</title>')
-    res.write('<body><h1>Hello Node.js!</h1></body>')
-    res.write('<p>This is Node.js sumple page.</p>')
-    res.write('<p>これは、Node.jsのサンプルページです。</p>', 'utf-8')
-    res.write('</body> </html >')
-    res.end();
+    fs.readFile('index.html', 'utf-8', (err, data) => {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        res.end();
+    });
 });
 
 server.listen(3000);
