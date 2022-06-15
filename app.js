2e428253a28afe53559010a8d1e481bf44d677a2
nakatomi <nakatomi369@gmial.com>
2022-06-16 00:20:10 +0900

    clean use sync method

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const http = require('http');
-const fs = require('fs').promises;
+const fs = require('fs');
 const ejs = require('ejs');
 
+const index_page = fs.readFileSync('./index.ejs', 'utf-8');
 
 const server = http.createServer(getFromClient);
 server.listen(3000);
 console.log("Server start!")
 
 async function getFromClient(req, res) {
-    const index_page = await fs.readFile('./index.ejs', 'utf-8');
     const content = ejs.render(index_page)
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write(content);
