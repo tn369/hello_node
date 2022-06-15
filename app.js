53cdd1b62c2d66a1c7b3060e4fd8aa62dcf982de
nakatomi <nakatomi369@gmial.com>
2022-06-16 00:10:38 +0900

    change use index.ejs

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const http = require('http');
 const fs = require('fs').promises;
+const ejs = require('ejs');
+
 
 const server = http.createServer(getFromClient);
 server.listen(3000);
 console.log("Server start!")
 
 async function getFromClient(req, res) {
+    const index_page = await fs.readFile('./index.ejs', 'utf-8');
+    const content = ejs.render(index_page)
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    const content = await fs.readFile('index.html', 'utf-8')
     res.write(content);
     res.end();
 }
